8b14627f5e41afa0daf31b41da043ea59b4c7127
jamesbeedy <jamesbeedy@gmail.com>
2020-05-20 03:47:18 +0000

    testing relations

---
+++ src/charm.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append("lib")
+
+import logging
+
+from ops.charm import CharmBase
+from ops.framework import Object
+from ops.main import main
+
+
+class FooProvides(Object):
+
+    def __init__(self, charm, relation_name):
+        super().__init__(charm, relation_name)
+
+        self.framework.observe(
+            charm.on[relation_name].relation_joined,
+            self.on_relation_joined
+        )
+
+    def on_relation_joined(self, event):
+        event.relation.data[self.model.unit]['foo'] = 'bar'
+
+
+class FooProviderCharm(CharmBase):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        self.foo = FooProvides(self, "foo")
+
+        self.framework.observe(self.on.start, self.on_start)
+        self.framework.observe(self.on.install, self.on_install)
+
+    def on_install(self, event):
+        pass
+
+    def on_start(self, event):
+        pass
+
+
+if __name__ == "__main__":
+    main(FooProviderCharm)
